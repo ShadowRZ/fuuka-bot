5f5e24d73e1d0b3cd10141532cb26d14e91dd49b
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2024-10-25 23:23:01 +0800

    refactor: Move to workspace

--- flake.nix
+++ flake.nix
@@ -66,18 +66,38 @@
           ]
         );
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
+        individualCrateArgs = commonArgs // {
+          inherit cargoArtifacts;
+          inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
+          # NB: we disable tests since we'll run them all via cargo-nextest
+          doCheck = false;
+        };
+
+        # Build *just* the cargo dependencies (of the entire workspace),
+        # so we can reuse all of that work (e.g. via cachix) when running in CI
+        # It is *highly* recommended to use something like cargo-hakari to avoid
+        # cache misses when building individual top-level-crates
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        fuuka-bot = craneLib.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts;
-          }
-        );
+        fileSetForCrate = crate: lib.fileset.toSource {
+          root = ./.;
+          fileset = lib.fileset.unions [
+            ./Cargo.toml
+            ./Cargo.lock
+            ./fuuka-bot
+            crate
+          ];
+        };
+
+        # Build the top-level crates of the workspace as individual derivations.
+        # This allows consumers to only depend on (and build) only what they need.
+        # Though it is possible to build the entire workspace as a single derivation,
+        # so this is left up to you on how to organize things
+        fuuka-bot = craneLib.buildPackage (individualCrateArgs // {
+          pname = "fuuka-bot";
+          cargoExtraArgs = "-p fuuka-bot";
+          src = fileSetForCrate ./fuuka-bot;
+        });
       in
       {
         checks = {
@@ -131,6 +151,24 @@
               partitionType = "count";
             }
           );
+
+          # Ensure that cargo-hakari is up to date
+          fuuka-bot-hakari = craneLib.mkCargoDerivation {
+            inherit src;
+            pname = "fuuka-bot-hakari";
+            cargoArtifacts = null;
+            doInstallCargoArtifacts = false;
+
+            buildPhaseCargoCommand = ''
+              cargo hakari generate --diff  # workspace-hack Cargo.toml is up-to-date
+              cargo hakari manage-deps --dry-run  # all workspace crates depend on workspace-hack
+              cargo hakari verify
+            '';
+
+            nativeBuildInputs = [
+              pkgs.cargo-hakari
+            ];
+          };
         };
 
         packages =
@@ -160,6 +198,7 @@
               pkgs.rust-analyzer
               pkgs.rustfmt
               pkgs.clippy
+              pkgs.cargo-hakari
             ];
 
             RUST_SRC_PATH = "${fenix'.stable.rust-src}/lib/rustlib/src/rust/library";
@@ -176,7 +215,7 @@
                 # Add additional build inputs here
                 pkgs.openssl
                 pkgs.sqlite
-                (fenix.packages.${system}.stable.withComponents [
+                (fenix'.stable.withComponents [
                   "cargo"
                   "clippy"
                   "rust-src"
