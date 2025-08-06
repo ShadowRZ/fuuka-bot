ac72527e29f8ef7bbd9b9648c6262cb6d73b75dd
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-08-06 16:11:41 +0800

    chore: Bump Rust to 1.88.0

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
               fenix = inputs'.fenix.packages;
               toolchain = fenix.fromToolchainFile {
                 dir = ./.;
-                sha256 = "sha256-KUm16pHj+cRedf8vxs/Hd2YWxpOrWZ7UOrwhILdSJBU=";
+                sha256 = "sha256-Qxt8XAuaUR2OMdKbN4u8dBJOhSHxS+uS06Wl9+flVEk=";
               };
 
               craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
