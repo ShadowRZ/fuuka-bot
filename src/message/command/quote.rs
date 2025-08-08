9212173670bcbd4562f06333531485c4e1e93a87
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-08-08 16:59:19 +0800

    chore: Migrate to let chains and Rust 1.88.0

--- src/message/command/quote.rs
+++ src/message/command/quote.rs
@@ -36,48 +36,47 @@ pub async fn process(
     if let Some(AnyTimelineEvent::MessageLike(AnyMessageLikeEvent::RoomMessage(
         RoomMessageEvent::Original(event),
     ))) = event
+        && let MessageType::Text(content) = event.content.msgtype
     {
-        if let MessageType::Text(content) = event.content.msgtype {
-            let member =
-                room.get_member(&event.sender)
-                    .await?
-                    .ok_or(crate::Error::UnexpectedError(
-                        "matrix-rust-sdk couldn't found the member in the room??",
-                    ))?;
-            let image = crate::render::quote::render(content, &member).await?;
-            let mut buf = Cursor::new(Vec::new());
-            image.write_to(&mut buf, ImageFormat::WebP)?;
+        let member = room
+            .get_member(&event.sender)
+            .await?
+            .ok_or(crate::Error::UnexpectedError(
+                "matrix-rust-sdk couldn't found the member in the room??",
+            ))?;
+        let image = crate::render::quote::render(content, &member).await?;
+        let mut buf = Cursor::new(Vec::new());
+        image.write_to(&mut buf, ImageFormat::WebP)?;
 
-            let data = buf.into_inner();
-            let size = data.len();
-            let res = room
-                .client()
-                .media()
-                .upload(&Mime::from_str("image/webp")?, data, None)
-                .await?;
+        let data = buf.into_inner();
+        let size = data.len();
+        let res = room
+            .client()
+            .media()
+            .upload(&Mime::from_str("image/webp")?, data, None)
+            .await?;
 
-            let (width, height) = (image.width(), image.height());
+        let (width, height) = (image.width(), image.height());
 
-            let mxc = res.content_uri;
-            let mimetype = "image/webp".to_string();
-            let mut thumb = ThumbnailInfo::new();
-            thumb.width = Some(width.into());
-            thumb.height = Some(height.into());
-            thumb.mimetype = Some(mimetype.clone());
-            thumb.size = Some(UInt::try_from(size)?);
-            let mut info = ImageInfo::new();
-            info.width = Some(width.into());
-            info.height = Some(height.into());
-            info.mimetype = Some(mimetype);
-            info.size = Some(UInt::try_from(size)?);
-            info.thumbnail_info = Some(Box::new(thumb));
-            info.thumbnail_source = Some(MediaSource::Plain(mxc.clone()));
-            let mut content = StickerEventContent::new("[Quote]".to_string(), info, mxc);
-            content.relates_to = Some(Relation::Reply {
-                in_reply_to: InReplyTo::new(event_id),
-            });
-            room.send(content).await?;
-        }
+        let mxc = res.content_uri;
+        let mimetype = "image/webp".to_string();
+        let mut thumb = ThumbnailInfo::new();
+        thumb.width = Some(width.into());
+        thumb.height = Some(height.into());
+        thumb.mimetype = Some(mimetype.clone());
+        thumb.size = Some(UInt::try_from(size)?);
+        let mut info = ImageInfo::new();
+        info.width = Some(width.into());
+        info.height = Some(height.into());
+        info.mimetype = Some(mimetype);
+        info.size = Some(UInt::try_from(size)?);
+        info.thumbnail_info = Some(Box::new(thumb));
+        info.thumbnail_source = Some(MediaSource::Plain(mxc.clone()));
+        let mut content = StickerEventContent::new("[Quote]".to_string(), info, mxc);
+        content.relates_to = Some(Relation::Reply {
+            in_reply_to: InReplyTo::new(event_id),
+        });
+        room.send(content).await?;
     }
 
     Ok(())
