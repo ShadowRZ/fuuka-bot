23fb8f5937292f7590e05133622d1b97f013ad57
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2025-08-25 12:41:41 +0800

    feat: Add Tokio Console and Journal logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use fuuka_bot::FuukaBot;
 use tracing::level_filters::LevelFilter;
-use tracing_subscriber::EnvFilter;
+#[cfg(not(feature = "tokio-console"))]
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -8,12 +10,27 @@ async fn main() -> anyhow::Result<()> {
         .add_directive(LevelFilter::WARN.into())
         .add_directive("fuuka_bot=debug".parse()?)
         .add_directive("matrix_sdk_crypto::backups=error".parse()?);
-    tracing_subscriber::fmt()
-        .with_env_filter(filter)
+    #[cfg(feature = "tokio-console")]
+    let filter = filter.add_directive("tokio=trace,runtime=trace".parse()?);
+
+    #[cfg(feature = "tokio-console")]
+    let console_layer = console_subscriber::spawn();
+    #[cfg(not(feature = "tokio-console"))]
+    let console_layer: Option<Layer<_>> = None;
+
+    #[cfg(feature = "use-journald")]
+    let logging_layer = tracing_journald::layer()?;
+    #[cfg(not(feature = "use-journald"))]
+    let logging_layer = tracing_subscriber::fmt::layer()
         .with_level(true)
         .with_target(true)
         .with_ansi(true)
-        .compact()
+        .compact();
+
+    tracing_subscriber::registry()
+        .with(console_layer)
+        .with(logging_layer)
+        .with(filter)
         .init();
 
     FuukaBot::from_config()?
