4154227a2bdf4b566d628aca9bff702249ce84bb
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-02-07 10:13:22 +0800

    chore(fmt): Reformat codes

--- src/services/github/mod.rs
+++ src/services/github/mod.rs
@@ -73,7 +73,7 @@ mod tests {
         ))
         .unwrap();
 
-        let error = super::Error(error.into());
+        let error = super::Error(error);
 
         let result = format!("{error}");
 
@@ -97,7 +97,7 @@ mod tests {
         ))
         .unwrap();
 
-        let error = super::Error(error.into());
+        let error = super::Error(error);
 
         let result = format!("{error}");
 
