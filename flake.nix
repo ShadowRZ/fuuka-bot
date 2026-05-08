86b1f4421232b54be5debe20a0b44766b13c8f36
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-05-08 15:16:43 +0800

    chore(flake): Override stdenv for aarch64-unknown-linux-musl devShell

--- flake.nix
+++ flake.nix
@@ -185,14 +185,20 @@
 
             RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
           };
-          aarch64-unknown-linux-musl = craneLib.devShell {
-            # Inherit inputs from checks.
-            checks = self.checks.${system};
+          aarch64-unknown-linux-musl =
+            (craneLib.devShell.override {
+              mkShell = pkgs.mkShell.override {
+                stdenv = pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv;
+              };
+            })
+              {
+                # Inherit inputs from checks.
+                checks = self.checks.${system};
 
-            packages = [
-              pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc
-            ];
-          };
+                packages = [
+                  pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc
+                ];
+              };
         };
       }
     );
